80f38c7ca4f170bbe2e9b49478ea41557993dd14
Nicholas Knoblauch <nwknoblauch@gmail.com>
2013-02-12 13:16:48 -0500

    First total rewrite

--- SAPSGSEA.R
+++ SAPSGSEA.R
@@ -12,12 +12,46 @@ phenotype <- runif(K,-1,1)*10
 
 correlated <- rnorm(N)
 
+
 D <- matrix(runif(N*K),N,K)
+rownames(D)<- paste0("gene_",1:N)
+
+
+
 D <- D+correlated%*%t(phenotype)
 
-g.cors <- rank(apply(D,MARGIN=1,function(x)cor(x,phenotype)))
+cors <- apply(D,MARGIN=1,function(x)cor(x,phenotype))
+S <- c(rownames(D)[order(cors)][1:13],tail(rownames(D)[order(cors)],n=2))
 
-#cutoff
-i <- 200
+L <- rank(cors)
+
+S.cors <- L[S]
+nS.cors <- L[!names(L)%in% S]
+
+
+N.r  <- sum(abs(S.cors))
+
+
+phit <- numeric(N)
+pmiss <- numeric()
+
+i <- 1
+for(i in 1:length(phit)){
+  if(names(L)[i]%in%S){
+    phit[i]<-phit[max(i-1,1)]+(L[i]/N.r)
+  }else{
+    phit[i]<-phit[max(i-1,1)]
+  }
+}
+
+pmiss <- numeric(N)
+for(i in 1:length(pmiss)){
+  if(!names(L)[i]%in%S){
+    pmiss[i]<- pmiss[max(i-1,1)]+(1/(N-length(S)))
+  }else{
+    pmiss[i] <- pmiss[max(i-1,1)]
+  } 
+}
+
 
 
